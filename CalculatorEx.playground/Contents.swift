9717306edca60d192711db18c78e434d3124ed32
yeggrrr <yjkim981103@gmail.com>
2024-03-07 18:36:45 +0900

    나만의 계산기 만들기(Lv3)_불필요한 주석제거

--- CalculatorEx.playground/Contents.swift
+++ CalculatorEx.playground/Contents.swift
@@ -1,5 +1,5 @@
 import Foundation
-// (Lv 1-2) + (Lv 3)
+// (Lv 3)
 class Calculator {
     var addOperation: AddOperation
     var substractOperation: SubstractOperation
@@ -12,34 +12,6 @@ class Calculator {
         self.multiplyOperation = multiplyOperation
         self.divideOperation = divideOperation
     }
-    
-//    enum Operator {
-//        case add
-//        case substract
-//        case multiply
-//        case divide
-//        case remainder
-//    }
-    
-//    func calculate(op: Operator, firstNum: Double, secondNum: Double) -> Double {
-//        switch op {
-//        case .add:
-//            return firstNum + secondNum
-//        case .substract:
-//            return firstNum - secondNum
-//        case .multiply:
-//            return firstNum * secondNum
-//        case .divide:
-//            if secondNum == 0 {
-//                return 0
-//            }
-//            
-//            return firstNum / secondNum
-//        case.remainder:
-//            return firstNum.truncatingRemainder(dividingBy: secondNum)
-//            
-//        }
-//    }
 }
 
 class AddOperation {
@@ -66,7 +38,6 @@ class DivideOperation {
     }
 }
 
-
 let addOperation = AddOperation()
 let substractOperation = SubstractOperation()
 let multiplyOperation = MultiplyOperation()
@@ -78,9 +49,3 @@ calculator.addOperation.operate(firstNum: 10, secondNum: 5)
 calculator.substractOperation.operate(firstNum: 10, secondNum: 5)
 calculator.multiplyOperation.operate(firstNum: 10, secondNum: 5)
 calculator.divideOperation.operate(firstNum: 10, secondNum: 5)
-
-//let addResult = calculator.calculate(op: .add, firstNum: 10, secondNum: 5)
-//let subtractResult = calculator.calculate(op: .substract, firstNum: 10, secondNum: 5)
-//let multiplyResult = calculator.calculate(op: .multiply, firstNum: 10, secondNum: 5)
-//let divideResult = calculator.calculate(op: .divide, firstNum: 10, secondNum: 5)
-//let remainderResult = calculator.calculate(op: .remainder, firstNum: 10, secondNum: 3)
