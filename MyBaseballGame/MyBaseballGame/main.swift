1ff0e6c690284c8e18d8fe53696de48b063121e6
yeggrrr <yjkim981103@gmail.com>
2024-03-13 15:27:17 +0900

    숫자 야구 게임 만들기(Lv1)

--- MyBaseballGame/MyBaseballGame/main.swift
+++ MyBaseballGame/MyBaseballGame/main.swift
@@ -1,11 +1,16 @@
-//
-//  main.swift
-//  MyBaseballGame
-//
-//  Created by YJ on 3/13/24.
-//
-
 import Foundation
+// 숫자 야구 게임 만들기 Lv1
 
-print("Hello, World!")
+// 숫자 3개 랜덤 숫자 담을 곳
+var randomNumbers:Set<Int> = []
+
+// 숫자 3개 랜덤으로 뽑는 코드 (while 문)
+while randomNumbers.count < 3 {
+    let num = Int.random(in: 1...9)
+    randomNumbers.insert(num)
+}
+print(randomNumbers) // 랜덤으로 나오는지 확인
+
+
+
 
